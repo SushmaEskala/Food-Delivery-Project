1e14c7791c3d1014cba3acd1d8b09043972e12d5
SushmaEskala <33688409+SushmaEskala@users.noreply.github.com>
2020-07-19 22:19:53 +0530

    data.sql

---
+++ data.sql
@@ -0,0 +1,11 @@
+#DML COMMANDS
+
+insert into Food_menu (sno,items,price) values (1,'Veg Manchuria', 50);
+insert into Food_menu (sno,items,price) values (2,'Death By Chocolate', 120);
+insert into Food_menu (sno,items,price) values (3,'Roti', 20);
+insert into Food_menu (sno,items,price) values (4,'Paneer Butter Masala', 150);
+insert into Food_menu (sno,items,price) values (5,'Cheese Burst Pizza', 200);
+
+insert into user values (101,'ram@gmail.com','Ram@123');
+insert into user values (102,'rahul@gmail.com','Rahul@123');
+insert into user values (103,'bunty@gmail.com','Bunty@123');
